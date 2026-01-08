6b4bfaba0772611ac733934772237479620b5754
afoncom <afon.com12@gmail.com>
2026-01-08 13:53:11 +0300

    Initial Commit

---
+++ CarsharingAfon/CarsharingAfonApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarsharingAfonApp.swift
+//  CarsharingAfon
+//
+//  Created by afon.com on 08.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct CarsharingAfonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
